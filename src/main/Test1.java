f7710d72c3e48067ff734b71c59089158e617929
Pankaj.prasad <pp5577488@gmail.com>
2025-06-13 16:30:38 +0530

    my first commit for git actions java application

---
+++ src/main/Test1.java
@@ -0,0 +1,11 @@
+public class Test1 {
+public String name(String str){
+    return str;
+}
+
+public static void main(String[] args) {
+    Test1 t=new Test1();
+    System.out.println("hello "+ t.name("java 24"));
+}
+    
+}
