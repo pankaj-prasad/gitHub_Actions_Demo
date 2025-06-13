e75d799b1b0bae921058ecab8384bb1088db0a9d
Pankaj.prasad <pp5577488@gmail.com>
2025-06-13 17:26:28 +0530

    added new class

--- src/main/Test1.java
+++ src/main/Test1.java
@@ -6,6 +6,7 @@ public String name(String str){
 public static void main(String[] args) {
     Test1 t=new Test1();
     System.out.println("hello "+ t.name("java 24"));
+    Test2.demo();
 }
     
 }
